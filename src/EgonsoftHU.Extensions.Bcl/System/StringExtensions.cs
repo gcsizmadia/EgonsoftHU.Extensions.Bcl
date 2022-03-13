8608adad4817d80aeb5f75e37f3570a24013500c
Gabor Csizmadia <gcsizmadia@users.noreply.github.com>
2022-03-13 01:40:43 +0100

    Update XML comments in StringExtensions.

--- src/EgonsoftHU.Extensions.Bcl/System/StringExtensions.cs
+++ src/EgonsoftHU.Extensions.Bcl/System/StringExtensions.cs
@@ -11,20 +11,20 @@ namespace EgonsoftHU.Extensions.Bcl
     public static class StringExtensions
     {
         /// <summary>
-        /// Indicates whether the specified string is null or an <see cref="String.Empty" /> string.
+        /// Indicates whether the specified string is <c>null</c> or <see cref="String.Empty"/>.
         /// </summary>
         /// <param name="value">The string to test.</param>
-        /// <returns>true if the value parameter is null or an empty string (""); otherwise, false.</returns>
+        /// <returns><c>true</c> if <paramref name="value"/> is <c>null</c> or <see cref="String.Empty"/>; otherwise, <c>false</c>.</returns>
         public static bool IsNullOrEmpty(this string value)
         {
             return String.IsNullOrEmpty(value);
         }
 
         /// <summary>
-        /// Indicates whether a specified string is null, empty, or consists only of white-space characters.
+        /// Indicates whether a specified string is <c>null</c>, <see cref="String.Empty"/>, or consists only of white-space characters.
         /// </summary>
         /// <param name="value">The string to test.</param>
-        /// <returns>true if the value parameter is null or System.String.Empty, or if value consists exclusively of white-space characters.</returns>
+        /// <returns><c>true</c> if <paramref name="value"/> is <c>null</c>, <see cref="String.Empty"/>, or if <paramref name="value"/> consists exclusively of white-space characters.</returns>
         public static bool IsNullOrWhiteSpace(this string value)
         {
             return String.IsNullOrWhiteSpace(value);
