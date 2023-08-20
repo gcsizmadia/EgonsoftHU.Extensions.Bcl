20a0690c836fc365708710c5de333595cd71dff0
Gabor Csizmadia <gcsizmadia@users.noreply.github.com>
2023-08-21 01:14:29 +0200

    Add DateTimeOffsetExtensions extension methods: ToMinutePrecision(), ToSecondPrecision()

---
+++ src/EgonsoftHU.Extensions.Bcl/System/DateTimeOffsetExtensions.cs
@@ -0,0 +1,37 @@
+﻿// Copyright © 2022-2023 Gabor Csizmadia
+// This code is licensed under MIT license (see LICENSE for details)
+
+using System;
+
+namespace EgonsoftHU.Extensions.Bcl
+{
+    /// <summary>
+    /// This class contains extension methods that are available for <see cref="DateTimeOffset"/> type.
+    /// </summary>
+    public static class DateTimeOffsetExtensions
+    {
+        /// <summary>
+        /// Removes the fractional seconds from the <paramref name="value"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="DateTimeOffset"/> value from which to remove the fractional seconds.</param>
+        /// <returns>A <see cref="DateTimeOffset"/> value with no fractional seconds.</returns>
+        public static DateTimeOffset ToSecondPrecision(this DateTimeOffset value)
+        {
+            TimeSpan timeOfDay = value.TimeOfDay;
+
+            return value.Subtract(timeOfDay).AddSeconds((int)timeOfDay.TotalSeconds);
+        }
+
+        /// <summary>
+        /// Removes the seconds from the <paramref name="value"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="DateTimeOffset"/> value from which to remove the seconds.</param>
+        /// <returns>A <see cref="DateTimeOffset"/> value with zero seconds.</returns>
+        public static DateTimeOffset ToMinutePrecision(this DateTimeOffset value)
+        {
+            TimeSpan timeOfDay = value.TimeOfDay;
+
+            return value.Subtract(timeOfDay).AddMinutes((int)timeOfDay.TotalMinutes);
+        }
+    }
+}
