d6136971d97170d3207be6d1c55e108bdaafd84c
Gabor Csizmadia <gcsizmadia@users.noreply.github.com>
2023-08-27 23:39:15 +0200

    Update parameter names for IsIn() and IsNotIn() methods.

--- src/EgonsoftHU.Extensions.Bcl/GenericExtensions.IEnumerable.cs
+++ src/EgonsoftHU.Extensions.Bcl/GenericExtensions.IEnumerable.cs
@@ -37,55 +37,55 @@ namespace EgonsoftHU.Extensions.Bcl
         /// Determines whether a specified collection contains the current value by using a specified <see cref="IEqualityComparer{T}"/>.
         /// If no equality comparer is specified then the default equality comparer will be used.
         /// </summary>
-        /// <typeparam name="T">The type of both the current value and the elements in <paramref name="acceptedValues"/>.</typeparam>
+        /// <typeparam name="T">The type of both the current value and the elements in <paramref name="values"/>.</typeparam>
         /// <param name="value">The value to locate in the collection.</param>
         /// <param name="equalityComparer">An equality comparer to compare values.</param>
-        /// <param name="acceptedValues">A collection in which to locate a value.</param>
+        /// <param name="values">A collection in which to locate a value.</param>
         /// <returns>Returns true if the specified collection contains an element that has the specified value; otherwise, false.</returns>
-        public static bool IsIn<T>(this T? value, IEqualityComparer<T?>? equalityComparer, params T?[] acceptedValues)
+        public static bool IsIn<T>(this T? value, IEqualityComparer<T?>? equalityComparer, params T?[] values)
         {
             return
                 equalityComparer is null
-                    ? acceptedValues.Contains(value)
-                    : acceptedValues.Contains(value, equalityComparer);
+                    ? values.Contains(value)
+                    : values.Contains(value, equalityComparer);
         }
 
         /// <summary>
         /// Determines whether a specified collection contains the current value by using the default equality comparer.
         /// </summary>
-        /// <typeparam name="T">The type of both the current value and the elements in <paramref name="acceptedValues"/>.</typeparam>
+        /// <typeparam name="T">The type of both the current value and the elements in <paramref name="values"/>.</typeparam>
         /// <param name="value">The value to locate in the collection.</param>
-        /// <param name="acceptedValues">A collection in which to locate a value.</param>
+        /// <param name="values">A collection in which to locate a value.</param>
         /// <returns>Returns true if the specified collection contains an element that has the specified value; otherwise, false.</returns>
-        public static bool IsIn<T>(this T? value, params T?[] acceptedValues)
+        public static bool IsIn<T>(this T? value, params T?[] values)
         {
-            return value.IsIn(equalityComparer: null, acceptedValues);
+            return value.IsIn(equalityComparer: null, values);
         }
 
         /// <summary>
         /// Determines whether a specified collection does not contain the current value by using a specified <see cref="IEqualityComparer{T}"/>.
         /// If no equality comparer is specified then the default equality comparer will be used.
         /// </summary>
-        /// <typeparam name="T">The type of both the current value and the elements in <paramref name="deniedValues"/>.</typeparam>
+        /// <typeparam name="T">The type of both the current value and the elements in <paramref name="values"/>.</typeparam>
         /// <param name="value">The value to locate in the collection.</param>
-        /// <param name="deniedValues">A collection in which to locate a value.</param>
+        /// <param name="values">A collection in which to locate a value.</param>
         /// <returns>Returns true if the specified collection does not contain an element that has the specified value; otherwise, false.</returns>
-        public static bool IsNotIn<T>(this T? value, params T?[] deniedValues)
+        public static bool IsNotIn<T>(this T? value, params T?[] values)
         {
-            return !value.IsIn(deniedValues);
+            return !value.IsIn(values);
         }
 
         /// <summary>
         /// Determines whether a specified collection does not contain the current value by using the default equality comparer.
         /// </summary>
-        /// <typeparam name="T">The type of both the current value and the elements in <paramref name="deniedValues"/>.</typeparam>
+        /// <typeparam name="T">The type of both the current value and the elements in <paramref name="values"/>.</typeparam>
         /// <param name="value">The value to locate in the collection.</param>
         /// <param name="equalityComparer">An equality comparer to compare values.</param>
-        /// <param name="deniedValues">A collection in which to locate a value.</param>
+        /// <param name="values">A collection in which to locate a value.</param>
         /// <returns>Returns true if the specified collection does not contain an element that has the specified value; otherwise, false.</returns>
-        public static bool IsNotIn<T>(this T? value, IEqualityComparer<T?>? equalityComparer, params T?[] deniedValues)
+        public static bool IsNotIn<T>(this T? value, IEqualityComparer<T?>? equalityComparer, params T?[] values)
         {
-            return !value.IsIn(equalityComparer, deniedValues);
+            return !value.IsIn(equalityComparer, values);
         }
     }
 }
