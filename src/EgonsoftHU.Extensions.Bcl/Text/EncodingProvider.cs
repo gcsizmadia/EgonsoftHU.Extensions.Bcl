fd9300f190fdca4ed1d1ed80acdedc3d9835bcd7
Gabor Csizmadia <gcsizmadia@users.noreply.github.com>
2024-08-30 00:20:47 +0200

    Add EncodingProvider class that provides an UTF8 encoding without the byte order mark.

---
+++ src/EgonsoftHU.Extensions.Bcl/Text/EncodingProvider.cs
@@ -0,0 +1,21 @@
+﻿// Copyright © 2022-2024 Gabor Csizmadia
+// This code is licensed under MIT license (see LICENSE for details)
+
+using System.Text;
+
+namespace EgonsoftHU.Extensions.Bcl.Text
+{
+    /// <summary>
+    /// An encoding provider, which supplies UTF-8 encoding without the Unicode byte order mark.
+    /// </summary>
+    public static class EncodingProvider
+    {
+        /// <summary>
+        /// The UTF-8 encoding without the Unicode byte order mark.
+        /// </summary>
+        /// <remarks>
+        /// <see cref="UTF8Encoding.GetPreamble"/> will return an empty byte array.
+        /// </remarks>
+        public static Encoding UTF8WithoutBOM { get; } = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
+    }
+}
