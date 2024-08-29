06b5369d430390bfc23cd087d2d4a8a8789b77c0
Gabor Csizmadia <gcsizmadia@users.noreply.github.com>
2024-08-30 00:25:59 +0200

    Add IEnumerableExtensions extension methods: DetectChanges<TItem, TKey>(); DetectChanges<TIncoming, TExisting, TKey>()

---
+++ src/EgonsoftHU.Extensions.Bcl/ItemsChangeResult.cs
@@ -0,0 +1,34 @@
+﻿// Copyright © 2022-2024 Gabor Csizmadia
+// This code is licensed under MIT license (see LICENSE for details)
+
+using System.Collections.Generic;
+
+namespace EgonsoftHU.Extensions.Bcl
+{
+    /// <summary>
+    /// Represents the difference between two sets of items as groups of new, existing and removed items.
+    /// </summary>
+    /// <typeparam name="TItem">The type of the items.</typeparam>
+    /// <param name="ItemsToAdd">Incoming items to add.</param>
+    /// <param name="ItemsToUpdate">Incoming items based on which to update existing items.</param>
+    /// <param name="ItemsToRemove">Existing items to be removed.</param>
+    public record ItemsChangeResult<TItem>(
+        IReadOnlyList<TItem> ItemsToAdd,
+        IReadOnlyList<TItem> ItemsToUpdate,
+        IReadOnlyList<TItem> ItemsToRemove
+    );
+
+    /// <summary>
+    /// Represents the difference between two sets of items as groups of new, existing and removed items.
+    /// </summary>
+    /// <typeparam name="TIncoming">The type of the incoming items.</typeparam>
+    /// <typeparam name="TExisting">The type of the existing items.</typeparam>
+    /// <param name="ItemsToAdd">Incoming items to add.</param>
+    /// <param name="ItemsToUpdate">Incoming items based on which to update existing items.</param>
+    /// <param name="ItemsToRemove">Existing items to be removed.</param>
+    public record ItemsChangeResult<TIncoming, TExisting>(
+        IReadOnlyList<TIncoming> ItemsToAdd,
+        IReadOnlyList<TIncoming> ItemsToUpdate,
+        IReadOnlyList<TExisting> ItemsToRemove
+    );
+}
--- src/EgonsoftHU.Extensions.Bcl/System/Collections/Generic/IEnumerableExtensions.cs
+++ src/EgonsoftHU.Extensions.Bcl/System/Collections/Generic/IEnumerableExtensions.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright © 2022-2024 Gabor Csizmadia
 // This code is licensed under MIT license (see LICENSE for details)
 
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
@@ -21,6 +22,100 @@ namespace EgonsoftHU.Extensions.Bcl
         public static bool IsNullOrEmpty<T>([NotNullWhen(false)] this IEnumerable<T>? sequence)
         {
             return sequence is null || !sequence.Any();
+        }
+
+        /// <summary>
+        /// Groups the difference between <paramref name="incomingItems"/> and <paramref name="existingItems"/>
+        /// into new, existing and removed items according to a specified key selector function.
+        /// </summary>
+        /// <typeparam name="TItem">The type of the items.</typeparam>
+        /// <typeparam name="TKey">The type of the key of an item.</typeparam>
+        /// <param name="existingItems">An <see cref="IEnumerable{TItem}"/> whose keys that are not also in <paramref name="incomingItems"/> will be returned as removed items.</param>
+        /// <param name="incomingItems">An <see cref="IEnumerable{TItem}"/> the elements of which will be returned either as new or existing items.</param>
+        /// <param name="keySelector">A function to extract the key for each element.</param>
+        /// <returns></returns>
+        public static ItemsChangeResult<TItem> DetectChanges<TItem, TKey>(
+            this IEnumerable<TItem>? existingItems,
+            IEnumerable<TItem>? incomingItems,
+            Func<TItem, TKey> keySelector
+        )
+        {
+            keySelector.ThrowIfNull();
+
+            IEnumerable<TItem> existing = existingItems ?? Array.Empty<TItem>();
+            IEnumerable<TItem> incoming = incomingItems ?? Array.Empty<TItem>();
+
+            IEnumerable<TKey> existingKeys = existing.Select(keySelector).ToList().AsReadOnly();
+            IEnumerable<TKey> incomingKeys = incoming.Select(keySelector).ToList().AsReadOnly();
+
+#if NET6_0_OR_GREATER
+            IEnumerable<TItem> itemsToAdd = incoming.ExceptBy(existingKeys, keySelector);
+            IEnumerable<TItem> itemsToUpdate = incoming.IntersectBy(existingKeys, keySelector);
+            IEnumerable<TItem> itemsToRemove = existing.ExceptBy(incomingKeys, keySelector);
+#else
+            IEnumerable<TKey> keysForAdd = incomingKeys.Except(existingKeys);
+            IEnumerable<TKey> keysForUpdate = incomingKeys.Intersect(existingKeys);
+            IEnumerable<TKey> keysForRemove = existingKeys.Except(incomingKeys);
+
+            IEnumerable<TItem> itemsToAdd = incoming.Join(keysForAdd, keySelector, key => key, (item, key) => item);
+            IEnumerable<TItem> itemsToUpdate = incoming.Join(keysForUpdate, keySelector, key => key, (item, key) => item);
+            IEnumerable<TItem> itemsToRemove = existing.Join(keysForRemove, keySelector, key => key, (item, key) => item);
+#endif
+            return
+                new ItemsChangeResult<TItem>(
+                    itemsToAdd.ToList().AsReadOnly(),
+                    itemsToUpdate.ToList().AsReadOnly(),
+                    itemsToRemove.ToList().AsReadOnly()
+                );
+        }
+
+        /// <summary>
+        /// Groups the difference between <paramref name="incomingItems"/> and <paramref name="existingItems"/>
+        /// into new, existing and removed items according to a specified key selector function.
+        /// </summary>
+        /// <typeparam name="TIncoming">The type of the incoming items.</typeparam>
+        /// <typeparam name="TExisting">The type of the existing items.</typeparam>
+        /// <typeparam name="TKey">The type of the key of an item.</typeparam>
+        /// <param name="existingItems">An <see cref="IEnumerable{TItem}"/> whose keys that are not also in <paramref name="incomingItems"/> will be returned as removed items.</param>
+        /// <param name="incomingItems">An <see cref="IEnumerable{TItem}"/> the elements of which will be returned either as new or existing items.</param>
+        /// <param name="existingKeySelector">A function to extract the key for each existing element.</param>
+        /// <param name="incomingKeySelector">A function to extract the key for each incoming element.</param>
+        /// <returns></returns>
+        public static ItemsChangeResult<TIncoming, TExisting> DetectChanges<TIncoming, TExisting, TKey>(
+            this IEnumerable<TExisting>? existingItems,
+            IEnumerable<TIncoming>? incomingItems,
+            Func<TExisting, TKey> existingKeySelector,
+            Func<TIncoming, TKey> incomingKeySelector
+        )
+        {
+            existingKeySelector.ThrowIfNull();
+            incomingKeySelector.ThrowIfNull();
+
+            IEnumerable<TExisting> existing = existingItems ?? Array.Empty<TExisting>();
+            IEnumerable<TIncoming> incoming = incomingItems ?? Array.Empty<TIncoming>();
+
+            IEnumerable<TKey> existingKeys = existing.Select(existingKeySelector).ToList().AsReadOnly();
+            IEnumerable<TKey> incomingKeys = incoming.Select(incomingKeySelector).ToList().AsReadOnly();
+
+#if NET6_0_OR_GREATER
+            IEnumerable<TIncoming> itemsToAdd = incoming.ExceptBy(existingKeys, incomingKeySelector);
+            IEnumerable<TIncoming> itemsToUpdate = incoming.IntersectBy(existingKeys, incomingKeySelector);
+            IEnumerable<TExisting> itemsToRemove = existing.ExceptBy(incomingKeys, existingKeySelector);
+#else
+            IEnumerable<TKey> keysForAdd = incomingKeys.Except(existingKeys);
+            IEnumerable<TKey> keysForUpdate = incomingKeys.Intersect(existingKeys);
+            IEnumerable<TKey> keysForRemove = existingKeys.Except(incomingKeys);
+
+            IEnumerable<TIncoming> itemsToAdd = incoming.Join(keysForAdd, incomingKeySelector, key => key, (item, key) => item);
+            IEnumerable<TIncoming> itemsToUpdate = incoming.Join(keysForUpdate, incomingKeySelector, key => key, (item, key) => item);
+            IEnumerable<TExisting> itemsToRemove = existing.Join(keysForRemove, existingKeySelector, key => key, (item, key) => item);
+#endif
+            return
+                new ItemsChangeResult<TIncoming, TExisting>(
+                    itemsToAdd.ToList().AsReadOnly(),
+                    itemsToUpdate.ToList().AsReadOnly(),
+                    itemsToRemove.ToList().AsReadOnly()
+                );
         }
     }
 }
