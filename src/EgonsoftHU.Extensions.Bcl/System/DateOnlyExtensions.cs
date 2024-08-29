72ac30f638e00d3167442fb448480133a28b31b1
Gabor Csizmadia <gcsizmadia@users.noreply.github.com>
2024-08-30 00:35:34 +0200

    Add DateOnly extension methods: IsInRange() - it makes use of the IntervalBoundsOptions enum type.

---
+++ src/EgonsoftHU.Extensions.Bcl/System/DateOnlyExtensions.cs
@@ -0,0 +1,46 @@
+﻿// Copyright © 2022-2024 Gabor Csizmadia
+// This code is licensed under MIT license (see LICENSE for details)
+
+#if NET6_0_OR_GREATER
+using System;
+
+using EgonsoftHU.Extensions.Bcl.Enumerations;
+
+namespace EgonsoftHU.Extensions.Bcl
+{
+    /// <summary>
+    /// This class contains extension methods that are available for <see cref="DateOnly"/> type.
+    /// </summary>
+    public static class DateOnlyExtensions
+    {
+        /// <summary>
+        /// Indicates whether the specified <see cref="DateOnly"/> value is in the specified interval.
+        /// </summary>
+        /// <param name="value">The value to compare against the lower bound and the upper bound.</param>
+        /// <param name="lowerBound">The beginning of the time interval.</param>
+        /// <param name="upperBound">The end of the time interval.</param>
+        /// <param name="options">Specifies whether to treat the lower and upper bounds as inclusive or exclusive. By default, both are treated as inclusive.</param>
+        /// <returns><see langword="true"/>, if <paramref name="value"/> is in the specified interval; otherwise, <see langword="false"/>.</returns>
+        public static bool IsInRange(
+            this DateOnly value,
+            DateOnly lowerBound,
+            DateOnly upperBound,
+            IntervalBoundsOptions options = default
+        )
+        {
+            return
+                (
+                    options.HasFlag(IntervalBoundsOptions.LeftOpen)
+                        ? value > lowerBound
+                        : value >= lowerBound
+                )
+                &&
+                (
+                    options.HasFlag(IntervalBoundsOptions.RightOpen)
+                        ? value < upperBound
+                        : value <= upperBound
+                );
+        }
+    }
+}
+#endif
