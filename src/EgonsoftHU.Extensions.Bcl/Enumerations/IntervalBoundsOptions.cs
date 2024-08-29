3ed11d7f6b0aba57b756a3fea049c9019c27f8f6
Gabor Csizmadia <gcsizmadia@users.noreply.github.com>
2024-08-30 00:33:32 +0200

    Add IntervalBoundsOptions enum type.

---
+++ src/EgonsoftHU.Extensions.Bcl/Enumerations/IntervalBoundsOptions.cs
@@ -0,0 +1,43 @@
+﻿// Copyright © 2022-2024 Gabor Csizmadia
+// This code is licensed under MIT license (see LICENSE for details)
+
+using System;
+
+namespace EgonsoftHU.Extensions.Bcl.Enumerations
+{
+    /// <summary>
+    /// Provides options how to treat the lower bound and the upper bound in an interval.
+    /// <para>
+    /// An interval can be:
+    /// <list type="bullet">
+    /// <item><see cref="Closed"/>: inclusive lower bound and inclusive upper bound</item>
+    /// <item><see cref="Open"/>: exclusive lower bound and exclusive upper bound</item>
+    /// <item><see cref="LeftOpen"/>: exclusive lower bound and inclusive upper bound</item>
+    /// <item><see cref="RightOpen"/>: inclusive lower bound and exclusive upper bound</item>
+    /// </list>
+    /// </para>
+    /// </summary>
+    [Flags]
+    public enum IntervalBoundsOptions
+    {
+        /// <summary>
+        /// Both the lower bound and the upper bound are included in the interval.
+        /// </summary>
+        Closed = 0,
+
+        /// <summary>
+        /// The lower bound is excluded from the interval, but the upper bound is included in the interval.
+        /// </summary>
+        LeftOpen = 1,
+
+        /// <summary>
+        /// The lower bound is included in the interval, but the upper bound is excluded from the interval.
+        /// </summary>
+        RightOpen = 2,
+
+        /// <summary>
+        /// Neither the lower bound nor the upper bound is included in the interval.
+        /// </summary>
+        Open = LeftOpen | RightOpen
+    }
+}
